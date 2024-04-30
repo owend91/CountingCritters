d3c50bcc7fcf6ced5a961ad67a876eb5c568173a
David Owen <owend91@gmail.com>
2024-04-30 14:10:14 -0600

    Initial Commit

---
+++ CountingCritters/CountingCrittersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountingCrittersApp.swift
+//  CountingCritters
+//
+//  Created by David Owen on 4/30/24.
+//
+
+import SwiftUI
+
+@main
+struct CountingCrittersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
